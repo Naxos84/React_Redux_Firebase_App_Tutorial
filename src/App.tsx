c0e9aa9c583045192adfc73d75f87dd1e50cf354
Matthias <matthias.kolley@googlemail.com>
2019-09-06 10:37:25 +0200

    lesson3
    setup everything

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
+
+export class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <h1>MarioPlan</h1>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
